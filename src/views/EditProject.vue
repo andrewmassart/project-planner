64aebee283ed6d59b0877609c99239ae87653c6a
Andrew Massart <amassart@asu.edu>
2020-12-12 16:00:16 -0500

    Updated data now saved to db.json file and shown in app

--- src/views/EditProject.vue
+++ src/views/EditProject.vue
@@ -1,5 +1,5 @@
 <template>
-  <form>
+  <form @submit.prevent="handleSubmit">
     <label>Title:</label>
     <input type="text" v-model="title" required />
     <label>Details:</label>
@@ -15,17 +15,28 @@ export default {
     return {
       title: "",
       details: "",
-      uri: 'http://localhost:3000/projects/' + this.id
+      uri: "http://localhost:3000/projects/" + this.id,
     };
   },
   mounted() {
-      fetch(this.uri)
-      .then(res => res.json())
-      .then(data => {
-          this.title = data.title
-          this.details = data.details
-      })
-  }
+    fetch(this.uri)
+      .then((res) => res.json())
+      .then((data) => {
+        this.title = data.title;
+        this.details = data.details;
+      });
+  },
+  methods: {
+    handleSubmit() {
+      fetch(this.uri, {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ title: this.title, details: this.details }),
+      }).then(() => {
+          this.$router.push('/')
+      }).catch(err => console.log(err))
+    },
+  },
 };
 </script>
 
