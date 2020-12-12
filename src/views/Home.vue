4a14cbd79d1e25aa923e0348065e3c086051495c
Andrew Massart <amassart@asu.edu>
2020-12-12 16:15:13 -0500

    Highlight active filter view link with prop: current

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="home">
+    <FilterNav @filterChange="current = $event" :current="current"/>
     <div v-if="projects.length">
       <div v-for="project in projects" :key="project.id">
-        <SingleProject :project="project" @delete="handleDelete" @complete="handleComplete" />
+        <SingleProject
+          :project="project"
+          @delete="handleDelete"
+          @complete="handleComplete"
+        />
       </div>
     </div>
   </div>
@@ -10,33 +15,35 @@
 
 <script>
 import SingleProject from "../components/SingleProject.vue";
+import FilterNav from "../components/FilterNav.vue";
 
 export default {
   name: "Home",
-  components: { SingleProject },
+  components: { SingleProject, FilterNav },
   data() {
     return {
       projects: [],
+      current: 'all'
     };
   },
   mounted() {
     fetch("http://localhost:3000/projects")
-      .then(res => res.json())
-      .then(data => (this.projects = data))
-      .catch(err => console.log(err.message));
+      .then((res) => res.json())
+      .then((data) => (this.projects = data))
+      .catch((err) => console.log(err.message));
   },
   methods: {
-      handleDelete(id) {
-          this.projects = this.projects.filter((project) => {
-              return project.id !== id
-          })
-      },
-      handleComplete(id) {
-          let p = this.projects.find(project => {
-              return project.id ==id
-          })
-          p.complete = !p.complete
-      }
-  }
+    handleDelete(id) {
+      this.projects = this.projects.filter((project) => {
+        return project.id !== id;
+      });
+    },
+    handleComplete(id) {
+      let p = this.projects.find((project) => {
+        return project.id == id;
+      });
+      p.complete = !p.complete;
+    },
+  },
 };
 </script>
