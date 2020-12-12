ee9a019f15381399051a7d537788491c1feec045
Andrew Massart <amassart@asu.edu>
2020-12-12 14:04:11 -0500

    boolean completed now toggles on clicking icon for db.json

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -4,8 +4,8 @@
       <h3 @click="showDetails = !showDetails">{{ project.title }}</h3>
       <div class="icons">
         <span class="material-icons"> edit </span>
-        <span @click ="deleteProject" class="material-icons"> delete_outline </span>
-        <span class="material-icons tick"> done_outline </span>
+        <span @click="deleteProject" class="material-icons"> delete_outline </span>
+        <span @click="toggleComplete" class="material-icons tick"> done_outline </span>
       </div>
     </div>
     <div v-if="showDetails" class="details">
@@ -20,16 +20,23 @@ export default {
   data() {
     return {
       showDetails: false,
-      uri: 'http://localhost:3000/projects/' + this.project.id
-    }
+      uri: "http://localhost:3000/projects/" + this.project.id,
+    };
   },
   methods: {
-      deleteProject() {
-          fetch(this.uri, { method: 'DELETE' })
-            .then(() => this.$emit('delete', this.project.id))
-            .catch(err => console.log(err.message))
-      }
-  }
+    deleteProject() {
+      fetch(this.uri, { method: "DELETE" })
+        .then(() => this.$emit("delete", this.project.id))
+        .catch((err) => console.log(err.message));
+    },
+    toggleComplete() {
+      fetch(this.uri, {
+        method: "PATCH",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ complete: !this.project.complete }),
+      });
+    },
+  },
 };
 </script>
 
@@ -51,12 +58,12 @@ h3 {
   align-items: center;
 }
 .material-icons {
-    font-size: 24;
-    margin-left: 4px;
-    color: #bbb;
-    cursor: pointer;
+  font-size: 24;
+  margin-left: 4px;
+  color: #bbb;
+  cursor: pointer;
 }
 .material-icons:hover {
-    color: #777;
+  color: #777;
 }
 </style>
