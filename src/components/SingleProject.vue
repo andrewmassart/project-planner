9adb15d6471e744816e0b8ed58dcf8d47f6a85e7
Andrew Massart <amassart@asu.edu>
2020-12-12 14:18:13 -0500

    complete changes color of left border onClick

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="project">
+  <div class="project" :class="{ complete: project.complete }">
     <div class="actions">
       <h3 @click="showDetails = !showDetails">{{ project.title }}</h3>
       <div class="icons">
@@ -21,7 +21,7 @@ export default {
     return {
       showDetails: false,
       uri: "http://localhost:3000/projects/" + this.project.id,
-    };
+    }
   },
   methods: {
     deleteProject() {
@@ -33,10 +33,12 @@ export default {
       fetch(this.uri, {
         method: "PATCH",
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ complete: !this.project.complete }),
-      });
-    },
-  },
+        body: JSON.stringify({ complete: !this.project.complete })
+      }).then(() => {
+          this.$emit('complete', this.project.id)
+          }).catch(err => console.log(err))
+    }
+  }
 };
 </script>
 
@@ -47,23 +49,32 @@ export default {
   padding: 10px 20px;
   border-radius: 4px;
   box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.05);
-  border-left: 4px solid #e90074;
+  border-left: 8px solid #e90074;
 }
+
 h3 {
   cursor: pointer;
 }
+
 .actions {
   display: flex;
   justify-content: space-between;
   align-items: center;
 }
+
 .material-icons {
   font-size: 24;
   margin-left: 4px;
   color: #bbb;
   cursor: pointer;
 }
+
 .material-icons:hover {
   color: #777;
+}
+
+/* change left border color for completed projects */
+.project.complete {
+    border-left: 8px solid #00ce89;
 }
 </style>
